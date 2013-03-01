4924224d3937822bf241ba9b102bfa4be63614a9
bcarter <github@idmsinc.net>
2013-03-01 15:21:28 -0700

    Working on adding unit tests

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -402,3 +402,14 @@ function CourseConflictCtrl($scope, $http, $parse, Data, Course) {
 
 //UsersCtrl.$inject = ['$scope', '$routeParams', $http, $q, 'Course'];
 
+
+function PhoneListCtrl($scope) {
+    $scope.phones = [
+        {"name": "Nexus S",
+            "snippet": "Fast just got faster with Nexus S."},
+        {"name": "Motorola XOOM™ with Wi-Fi",
+            "snippet": "The Next, Next Generation tablet."},
+        {"name": "MOTOROLA XOOM™",
+            "snippet": "The Next, Next Generation tablet."}
+    ];
+}
